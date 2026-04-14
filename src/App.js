a0c6cbaa6f230889cadf360ee9991767004b2d7a
Ingrid <ingridscalant@gmail.com>
2026-04-13 23:45:25 -0400

    working 1st draft, no comp vision

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import PhotoBooth from "./components/photobooth"
+import "./styles/global.css"
+
+//lconst logoSrc = ""
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      minHeihgt: "100vh",
+      display: "flex", 
+      flexDirection: "column",
+      alignItems: "center"
+    }}>
+      <div style = {{
+        width: "100%", 
+        maxWidth: 1200, 
+        display: "flex",
+        alignItems: "center", 
+        gap: 12, 
+        padding: "20px 32px"
+      }}>
+      {/* <img src={logoSrc} alt="JiggleDuo Logo" style={{ width: 50 }} />*/}
+       <h1 style={{
+          fontFamily: "AOT",
+          color: "#41713e",
+          margin: "50px auto",
+          textAlign: "center",
+          fontSize: "3.5rem",
+          width: "100%"
+        }}>
+          AttackOnTitan Booth
+        </h1> 
+      </div>
+
+      <div style={{
+        flex: 1,
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: "40px"
+      }}
+      >
+
+      <PhotoBooth />
+      </div>
     </div>
   );
 }
